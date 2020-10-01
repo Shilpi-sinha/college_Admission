98493bf17fb1a65582eb2ce25f4db053fd5bec48
Shilpi Sinha <68265036+Shilpi-sinha@users.noreply.github.com>
2020-10-01 18:06:47 +0530

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ShilpiConfig(AppConfig):
+    name = 'shilpi'
---
+++ views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def result(request):
+    val1=int(request.POST["num"])
+    val2=(request.POST["var"])
+    if val1>70:
+        text="yes, you are selected"
+    
+        return HttpResponse(text)
+        #render('home.html', text='Yes you are selected')       
+    elif(val2=="yes"):
+        text="yes, you are selected"
+    
+        return HttpResponse(text)
+    
+        #return render('home.html', text='Yes you are selected')
+        
+    else:
+        text="Sorry, you are not selected"
+    
+        return HttpResponse(text)
+        #return render('home.html', text='Sorry,you are not selected')
+
+    
+    
+
+
